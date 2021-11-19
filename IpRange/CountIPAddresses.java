60884efedb7dec14c141b0475ca0cf88f6b2c3dd
Arun Krishnan <arun4444@gmail.com>
2021-11-19 06:50:05 -0500

    Ip address v2

--- IpRange/CountIPAddresses.java
+++ IpRange/CountIPAddresses.java
@@ -1,29 +1,19 @@
 package CodeDojo.IpRange;
 
-import java.util.Stack;
-
 public class CountIPAddresses {
-	private static Stack<Long> ipToStack(String ip) {
-		Stack<Long> ipStack = new Stack<Long>();
-		final String[] ipElements = ip.split("\\.");
-		for (String element : ipElements) {
-			ipStack.add(Long.parseLong(element));
-		}
-		return ipStack;
-	}
-
 	public static long ipsBetween(String start, String end) {
-		Stack<Long> startIp = ipToStack(start);
-		Stack<Long> endIp = ipToStack(end);
+		final String[] startIp = start.split("\\.");
+		final String[] endIp = end.split("\\.");
 		long result = 0;
 		long multiplicand = 1;
-		int size = startIp.size();
-		for (int i = 0; i < size; i++) {
-			if (i > 0) {
+		int size = startIp.length;
+		int power = 0;
+		for (int i = size - 1 ; i >= 0; i--) {
+			if (power > 0) {
 				multiplicand = multiplicand * 256;
 			}
-			long subFrom = endIp.pop();
-			long sub = startIp.pop();
+			long subFrom = Long.parseLong(endIp[i]);
+			long sub = Long.parseLong(startIp[i]);
 			boolean borrow = false;
 			if (subFrom < sub) {
 				borrow = true;
@@ -31,11 +21,12 @@ public class CountIPAddresses {
 			}
 			long diffrence = subFrom - sub;
 			result += diffrence * multiplicand;
-			if (startIp.size() > 0 && borrow) {
-				long borrowAdd = startIp.pop();
+			if (i > 0 && borrow) {
+				Long borrowAdd = Long.parseLong(startIp[i-1]);
 				borrowAdd++;
-				startIp.push(borrowAdd);
+				startIp[i-1] = borrowAdd.toString();
 			}
+			power++;
 		}
 		return result;
 	}
