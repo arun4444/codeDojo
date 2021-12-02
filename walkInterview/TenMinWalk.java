3795f178e3fa538b5618f114cd263b271e12dc20
Arun Krishnan <arun4444@gmail.com>
2021-12-01 21:51:44 -0500

    consts

--- walkInterview/TenMinWalk.java
+++ walkInterview/TenMinWalk.java
@@ -1,17 +1,22 @@
 import java.util.*;
 
 public class TenMinWalk {
+  private static final Character north = 'n';
+  private static final Character south = 's';
+  private static final Character east = 'e';
+  private static final Character west = 'w';
+
   public static boolean isValid(char[] walk) {
     Map<Character, Integer> countMap = getCountMap(walk);
-    return (walk.length == 10 && countMap.get('s') == countMap.get('n') && countMap.get('w') == countMap.get('e'));
+    return (walk.length == 10 && countMap.get(south) == countMap.get(north) && countMap.get(west) == countMap.get(east));
   }
 
   private static Map<Character, Integer> getCountMap(char[] walk) {
     Map<Character, Integer> countMap = new HashMap<>();
-    countMap.put('n', 0);
-    countMap.put('s', 0);
-    countMap.put('e', 0);
-    countMap.put('w', 0);
+    countMap.put(north, 0);
+    countMap.put(south, 0);
+    countMap.put(east, 0);
+    countMap.put(west, 0);
     for (char c : walk) {
       countMap.put(c, countMap.get(c) + 1);
     }
